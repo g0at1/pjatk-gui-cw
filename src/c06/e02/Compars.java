b21a3265c96e1b44956047eaa1af26403c1a6e38
g0at1 <michal.lendzion@vp.pl>
2023-04-09 22:32:44 +0200

    Exercise 06-02

---
+++ src/c06/e02/Compars.java
@@ -0,0 +1,30 @@
+package c06.e02;
+
+import java.util.Arrays;
+
+public
+    class Compars {
+
+    public static void main(String[] args) {
+        Integer[] a = {1,5,33,12,98,15};
+        printTable("Original    ", a);
+
+        Arrays.sort(a,new MyComp(MyComp.BY_VAL));
+        printTable("ByVal       ", a);
+
+        Arrays.sort(a,new MyComp(MyComp.BY_VAL_REV));
+        printTable("ByValRev    ", a);
+
+        Arrays.sort(a,new MyComp(MyComp.BY_NUM_OF_DIVS));
+        printTable("ByNumOfDivs ", a);
+
+        Arrays.sort(a,new MyComp(MyComp.BY_SUM_OF_DIGS));
+        printTable("BySumOfDigs ", a);
+    }
+
+    static void printTable(String mess, Integer[] a) {
+        System.out.print(mess + "[ ");
+        for (int d : a) System.out.print(d + " ");
+        System.out.print("]\n");
+    }
+}
